98de5c1ad67508d699525b08ca4eb9abad9364ba
dearmai <dearmai@paran.com>
2012-11-04 03:05:32 +0900

    Initial Commit

---
+++ ShortestPath/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ShortestPath
+//
+//  Created by Yoo Rinjae on 12. 11. 4..
+//  Copyright (c) 2012년 DearMai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
